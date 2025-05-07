219745fad31d7eea124e5562da28caf1eda3fba8
Sylvain <sylvainzhng@gmail.com>
2025-05-07 14:35:46 +0200

    Update nav

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 window.onscroll = function changeNavImage() {
     if (window.scrollY >= 100) {
-        document.getElementById("nav-logo").src = "img/axone_black.png";
+        document.getElementById("nav-logo").src = "/img/axone_black.png";
     }
     else {
-        document.getElementById("nav-logo").src = "img/axone_white.png";
+        document.getElementById("nav-logo").src = "/img/axone_white.png";
     }
 }
