87089700e1ea043a5eb02d7bafebdcaf4781794b
Sylvain <sylvainzhng@gmail.com>
2025-05-14 16:12:57 +0200

    update

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 window.onscroll = function changeNavImage() {
     if (window.scrollY >= 100) {
-        document.getElementById("nav-logo").src = "{{ '/img/axone_black.png' | prepend: site.baseurl | prepend: site.url }}";
+        document.getElementById("nav-logo").src = "../img/axone_black.png";
     }
     else {
-        document.getElementById("nav-logo").src = "{{ '/img/axone_white.png' | prepend: site.baseurl | prepend: site.url }}";
+        document.getElementById("nav-logo").src = "../img/axone_white.png";
     }
 }
